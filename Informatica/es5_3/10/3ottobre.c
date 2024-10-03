7f662b6e4f31059e469e3fa1b3f21ec1b0c2c9cf
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2024-10-03 06:46:40 +0000

    es2_3ottobre.c

---
+++ Informatica/es5_3/10/3ottobre.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    int c;
+    int minore;
+    int medio;
+    int maggiore;
+    printf("Scrivi un valore");
+    scanf("d%:", &a);
+    printf("Scrivi un valore");
+    scanf("%d:", &b);
+    printf("Scrivi un valore";
+    scanf("%d:", c);
+    a=0;
+    b=0,
+    c=0;
+    if (a>b&&b>c){
+        printf("Il maggiore è:%d", a)
+        a==maggiore
+    else
+        if (a<b&&b>c)
+            printf("Il minore è:%d", a);
+            A==minore
+    }
+    if (b>a&&a>c){
+        printf("Il maggiore è:%d", b)
+        b==maggiore
+    else
+        if (b<a&&a<c)
+        printf("Il minore è:%d, b")
+        b==minore
+    }
+    if (c>a&&a>b)
+        printf("Il maggiore è:%d", c)
+        c==maggiore
+    else
+        If (c<a&&a<b)
+        c==minore
+    }
+    if (a==maggiore)
+    
+
+
+
+
+    }
+
+
+}
