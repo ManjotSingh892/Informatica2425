da7b8dde7b629d194541ff53d7dd08a6472a5310
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2024-10-07 11:06:58 +0000

    modifiche

--- compiti/es2_percasa_30sett.c
+++ compiti/es2_percasa_30sett.c
@@ -35,6 +35,6 @@ int main() {
                     printf("C è il massimo\n");
 
     }
-            printf("La media aritmetica è: %f\n", media);
+            printf("La media aritmetica è: %.2f\n", media);
     return 0;
 }
