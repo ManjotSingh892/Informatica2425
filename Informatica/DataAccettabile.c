aec868de5616c7691b1c42281750a51f7a27b6f1
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2024-10-08 11:54:13 +0000

    DataAccettabile.c

---
+++ Informatica/DataAccettabile.c
@@ -0,0 +1,61 @@
+/*Data una data in formato gg mm aaaa
+ verificare se la data è accettabile*/
+ #include <stdio.h>
+ int main(){
+    int gg;
+    int mm;
+    int anno;
+    int bisestile=0;
+    gg=0;
+    mm=0;
+    anno=0;
+    
+        printf("scrivi il giorno");
+            scanf("%d", &gg);
+        printf("scrivi il mese");
+            scanf("%d", &mm);
+        printf("scrivi il anno");
+            scanf("%d", &anno);
+        //CONTROLLO// //bisistile QUANDO MULTIPLO DI 4 MA NON DI 100 E 400// //if (anno%4==0&&anno%100!=0&&anno%400!=0)//
+        if(anno%100==0){
+            if (anno%400==0){
+                printf("L'anno è bisestile");
+                bisestile=1;
+            }
+        }
+        else{
+            if (anno%4==0){
+                printf("L'anno non è bisestile");
+                bisestile=1;
+            }
+            //controllo del mese//
+            if(mm>0&&mm<13){
+                if (mm==2){
+                    if(gg>=1 && gg<=28+bisestile){
+                        printf("\nLa data è accettabile");
+                    }
+                }
+            else{
+                if(mm==11 || mm==4 ||mm==6 ||mm==9){
+                    if (gg>=1 && gg<= 30){
+                        printf("\nLa data è accettabile");
+                    }
+                }
+            }
+            }
+        }
+       
+
+
+            
+
+
+
+
+
+    return 0;
+
+
+
+
+ }
--- Informatica/es5_3/10/2_7_10_2024.c
+++ Informatica/es5_3/10/2_7_10_2024.c
@@ -15,9 +15,9 @@ int main (){
     cifra2=(numero%100);
     cifra3=(numero%1000);
     somma=(cifra1+cifra2+cifra3);
-    if (somma % 3)=0{
+    if (somma !=0 )
         printf("Il risultato è divisibile per 3, quindi è dispari");
-    }
+    
     if (somma % 3)!=0
         printf("Il risultato non è divisibile per 3, quindi non è dispari");
     return 0;
