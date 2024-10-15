897ff0e3d789ac5cad04084c84600c8e364ac285
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2024-10-15 11:57:34 +0000

    correzione es date recenti

--- compiti/10ottobreespercasasettimanaprossima.c
+++ compiti/10ottobreespercasasettimanaprossima.c
@@ -8,6 +8,9 @@ int main(){
     int g2;
     int m2;
     int a2;
+    int bisestile=0;
+
+    //richiesta date//
         printf ("Inserisci il giorno della data numero 1\n");
         scanf ("%d", &g);
         printf ("Inserisci il mese della data numero 1\n");
@@ -21,7 +24,52 @@ int main(){
         scanf ("%d", &m2);
         printf ("Inserisci l'anno della data numero 2\n");
         scanf ("%d", &a2);
-        
+//controllo data//
+
+    
+        //CONTROLLO// //bisistile QUANDO MULTIPLO DI 4 MA NON DI 100 E 400// //if (anno%4==0&&anno%100!=0&&anno%400!=0)//
+        if(a%100==0){
+            if (a%400==0){
+                printf("L'anno è bisestile\n");
+                bisestile=1;
+            }
+        }
+        else{
+            if (a%4==0){
+                printf("L'anno non è bisestile\n");
+                bisestile=1;
+            }
+            //controllo del mese//
+            if(m>0&&m<13){
+                if (m==2){
+                    if(g>=1 && g<=28+bisestile){
+                        printf("\nLa data è accettabile\n");
+                    }
+                }
+            else{
+                if(m==11 || m==4 ||m==6 ||m==9){
+                    if (g>=1 && g<=30){
+                        printf("La data è accettabile\n");
+                    }
+                    else{
+                        printf("la data non è accettabile\n");
+                    }
+                    if (g>=1 && g<= 31){
+                        printf("La data è accettabile\n");
+                    }
+                    else{
+                        printf("La data non è accettabile\n");
+                    }
+                }
+            }
+            }
+        }
+    
+
+
+
+
+
         if(a>a2)
             printf("La prima data è la più recente\n");
         if(a==a2)
