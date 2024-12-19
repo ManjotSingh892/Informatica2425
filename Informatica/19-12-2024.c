24fceeac391a29a50accc1f18f04cc77bf01415e
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2024-12-19 09:47:28 +0000

    ok

--- Informatica/19-12-2024.c
+++ Informatica/19-12-2024.c
@@ -4,22 +4,13 @@ int main(){
     int n=0;
     int i=1;
     int quadrato=0;
-
     do{printf("Inserisci un valore da assegnare ad N\n");
     scanf("%d", &n);
     }while(n<1);
     printf("Il quadrato è: %d\n", n*n);
-    
     for(int cnt=1; cnt<=n; cnt++){
-      
         quadrato=quadrato+i;
         i=i+2;
-
     }printf("Il quadrato con numeri dispari è: %d\n", quadrato);
-
-
-
-
-
     return 0;
 }
