a3641f564f0b5c4bc315dbdad906b830641c4ce6
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2025-02-06 21:15:19 +0000

    correzione esercizio cerchio

--- Funzioni C/RaggioCerchioPerArea.c
+++ Funzioni C/RaggioCerchioPerArea.c
@@ -1,30 +1,33 @@
 /*Dato il raggio calcolare la circonferenza e l'area del cerchio*/
 #include <stdio.h>
-const float  pi=3.14;
-void circonferenza(int *circonferenza);
-void areacerchio(int *area);
-int main(){
-    int r=0;
-    int area=0;
-    int circonferenza=0;
-        do{
-            printf("Inserisci un valore da dare al raggio\n");
-            scanf("%d", &r);
-        }while(r<=0);
-        
-        area=areacerchio(*area);
-        printf("Area: %d", area);
-        circonferenza=circonferenza(int *circonferenza);
-        printf("Circonferenza: %d", circonferenza);
+const float pi = 3.14;
+
+void circonferenza(int r, float *circonferenza);
+void areacerchio(int r, float *area);
+
+int main() {
+    int r = 0;
+    float area = 0;
+    float circonferenza = 0;
+
+    do {
+        printf("Inserisci un valore da dare al raggio\n");
+        scanf("%d", &r);
+    } while (r <= 0);
+
+    areacerchio(r, &area);
+    printf("Area: %.2f\n", area);
+
+    circonferenza(r, &circonferenza);
+    printf("Circonferenza: %.2f\n", circonferenza);
 
     return 0;
 }
-void circonferenza(int *circonferenza){
-    *circonferenza=2*pi*r;
-    return *;
-}
-void areacerchio(int *area){
-    *area=pi*(r*r);
-    return *area;
 
+void circonferenza(int r, float *circonferenza) {
+    *circonferenza = 2 * pi * r;
+}
+
+void areacerchio(int r, float *area) {
+    *area = pi * (r * r);
 }
