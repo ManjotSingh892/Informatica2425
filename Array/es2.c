555c96b37614aafc1b310d60ca5520f6be1b7950
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2025-03-27 09:23:42 +0000

    dfghj+

--- Array/es2.c
+++ Array/es2.c
@@ -1,8 +1,24 @@
-/*Caricato un vettore random calcolare la media dei valori*/
 #include <stdio.h>
-#include "libreria.c"
+#include <stdlib.h>
+#include <time.h>
+#include <libreria.h>
 
-int main(){
+float calcolamedia(int v[], int n) {
+    float somma = 0;
+    for (int i = 0; i < n; i++){
+        somma= somma+v[i]; 
+    }
 
-    
+    return somma/n;
+}
+
+int main() {
+    int v[31];
+    srand(time(NULL));
+    for (int i = 0; i < 31; i++) {
+        v[i] = rand() % 101;
+        printf("%d ", v[i]);
+    }
+    printf("\nMedia: %f\n", calcolamedia(v, 31));
+    return 0;
 }
