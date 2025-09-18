28906ff86462012ae2b9f498c8e8fae3a837061a
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2025-09-18 07:52:22 +0000

    esercizio matrice spiarale da correggere perchè ci sono degli errori

--- Esercizi/Es_2_Matrice_Spirale.c
+++ Esercizi/Es_2_Matrice_Spirale.c
@@ -2,22 +2,17 @@
 /*1 2 3 
   8 9 4
   7 6 4 */
-
-
 #include <stdio.h>
 #define dim 5
 void mostra(int matrice[dim][dim], int righe, int colonne) {
-    int r;
-    int c;
-    for (r=0;r<righe;r++) {
-        for (c=0;c<colonne;c++) {
+    for (int r=0;r<righe;r++) {
+        for (int c=0;c<colonne;c++) {
             printf("%d ", matrice[r][c]);
-        }
-        printf("\n");
+        }printf("\n");
     }
 }
 void riempi(int matrice[dim][dim], int righe, int colonne) {
-    int numero = 1
+    int numero=1;
     int alto=0;
     int basso=righe-1;
     int sinistra=0;
@@ -48,10 +43,11 @@ void riempi(int matrice[dim][dim], int righe, int colonne) {
 }
 int main() {
     int matrice[dim][dim];
-    int righe, colonne;
+    int righe;
+    int colonne;
     printf("Righe max %d\n", dim);
     scanf("%d", &righe);
-    printf("Colone max %d\n", dim);
+    printf("Colonne max %d\n", dim);
     scanf("%d", colonne);
     riempi(matrice, righe, colonne);
     mostra(matrice, righe, colonne);
