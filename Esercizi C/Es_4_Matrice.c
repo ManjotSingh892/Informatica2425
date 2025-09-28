ac7d78999b92c9bd0f74b8c8a7174072039c59be
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2025-09-28 15:45:37 +0000

    3rr3g3rt4r43r

--- Esercizi C/Es_4_Matrice.c
+++ Esercizi C/Es_4_Matrice.c
@@ -1,12 +1,11 @@
-/*
-Scrivere un programma in linguaggio C che:
-    1. Allochi dinamicamente un array iniziale di N numeri
-    2. Inserimento dei valori
-    3. Stampa array
-    4. Calcoli la somma degli elementi multipli di 3
-    5. Creiamo un nuovo array contenente solo valori dispari
+/* Esempio di malloc e realloc insieme
+    Scrivere un programma in linguaggio C che:
+    1. Allochi dinamicamente un array iniziale di n numeri interi con malloc;
+    2. inserimento dei valori;
+    3. stampi l'array;
+    4. calcoli la somma dei valori multipli di 3;
+    5. creiamo un nuovo array con solo i numeri dispari
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 int* creaVett(int _n){
@@ -14,57 +13,56 @@ int* creaVett(int _n){
     int *_vettore=(int*)malloc(_n *sizeof(int));
     if(_vettore==NULL){
         printf("Errore di allocazione\n");
-        return 1;
+        exit(1);
     }
     return _vettore;
 }
+int* inserisciValori(int *_vettore, int _n){
 
-int * inserisciValori(int *_vettore, int _n){
-
-    for(int i=0, i_<n; i++){
+    for(int i=0; i<_n; i++){
         printf("Inserisci l'elemento: ");
         scanf("%d", &_vettore[i]);
     }
     return _vettore;
 }
-
 void stampaVett(int *_vettore, int _n){
 
     printf("Hai inserito i seguenti elementi: ");
-
     for(int i=0; i<_n; i++){
-        printf("%d", array[i]);
+        printf("%d ", _vettore[i]);
     }
-    printf("\n")
+    printf("\n");
 }
-
-void sommaMultipli(){
+int* sommaMultipli(int *_vettore, int _n, int *somma){
     int *nuovoArray=NULL;
-    int somma=0;
+    int cnt=0;
+    *somma=0;
     for(int i=0; i<_n; i++){
-        int cnt=0;
-        if(*vettore[i]%3==0){
-            somma=somma+ *vettore[i];
-            *nuovoArray=(int *)realloc(nuovoArray, cnt*sizeof(int));
-            *nuovoArray= *vettore[i];
+        if(_vettore[i]%3==0){
+            *somma += _vettore[i];
+        }
+        if(_vettore[i]%2!=0){
+            nuovoArray=(int*)realloc(nuovoArray, (cnt+1)*sizeof(int));
+            nuovoArray[cnt]=_vettore[i];
+            cnt++;
         }
     }
+    return nuovoArray;
 }
-
-
 int main(){
-    int *vettore=NULL; //puntatore al vettore
+    int *vettore=NULL;
     int *nuovoArray=NULL;
     int n;
+    int somma=0;
     do{
-        printf("Inserisci la quantità degli elementi: ");
-        scanf("%d, &n");
+        printf("Inserisci la quantità degli elementi");
+        scanf("%d", &n);
     }while(n<0);
-
-    vettore= creaVett(n);
-    vettore= inserisciValori(vettore, n);
+    vettore=creaVett(n);
+    vettore=inserisciValori(vettore, n);
     stampaVett(vettore, n);
-    
-
+    nuovoArray=sommaMultipli(vettore, n, &somma);
+    printf("Somma dei multipli di, 3 è : %d\n", somma);
     free(vettore);
+    free(nuovoArray);
 }
