eb0987e520b640349872f6245fdab5fa7f403351
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2025-10-05 14:03:27 +0000

    wdfwffwefe

--- Esercizi C/Es_5_Matrice.c
+++ Esercizi C/Es_5_Matrice.c
@@ -3,7 +3,7 @@
 Scrivi un programma in C che:
     1. Chiede all'utente quanti numeri interi vuole inserire;
     2. Alloca dinamicamente un array di quella dimensione con malloc();
-    3. Permette al'utente di inserire i numeri;
+    3. Permette all'utente di inserire i numeri;
     4. Chiede all'utente se vuole aumentare la dimensione dell'Array,
         - Se sì, richiede la nuova dimensione e usa realloc() per ridimensionarlo;
         - Permette quindi di insirire i nuovi valori negli spazi aggiunti;
@@ -11,75 +11,85 @@ Scrivi un programma in C che:
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void Aumento(){
-    int flag=0;
-    do{
-        printf("Vuoi aumentare la dimensione dell'Array?\n");
-        printf("Inserisci: \n 1. Si\n 0: No\n");
-        scanf("%d", &flag);
-    }while(flag!=0||flag!=1);
-    if(flag==0){
-        printf("Hai deciso di non aggiungere altri elementi\n");
-    }
-    else {
-        richiesta();
-        Allocamento();
-        stampaVett(vettore, n);
-
-    }
-
+int richiesta() {
+    int n;
+    do {
+        printf("Quanti numeri vuoi inserire: ");
+        scanf("%d", &n);
+        if (n <= 0)
+            printf("Numero nn valido, riprova\n");
+    } while (n <= 0);
+    return n
 }
 
-void richiesta(){
-do{
-    printf("Quanti numeri vuoi inserire\n");
-    scanf("%d", & *n);
-}while(*n<0);
-
-}
-
-void Allocamento(vettore, n){
-int *vettore=(int*)malloc(n *sizeof(int));
-}
-
-int* creaVett(int n){
-
-    int *vettore=(int*)malloc(n *sizeof(int));
-    if(vettore==NULL){
-        printf("Errore di allocazione\n");
+int* creaVett(int n) {
+    int *vettore = (int*)malloc(n * sizeof(int));
+    if (vettore == NULL) {
+        printf("Errore allocazione memoria\n");
         exit(1);
     }
     return vettore;
 }
 
-int* inserisciValori(int *vettore, int n){
-
-    for(int i=0; i<n; i++){
-        printf("Inserisci l'elemento: ");
+void inserisciValori(int *vettore, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("Elemento %d: ", i + 1);
         scanf("%d", &vettore[i]);
     }
-    return vettore;
 }
 
-void stampaVett(int *vettore, int n){
-
-    printf("Hai inserito i seguenti elementi: ");
-    for(int i=0; i<n; i++){
+void stampaVett(int *vettore, int n) {
+    printf("\nArray finale:\n");
+    for (int i = 0; i < n; i++) {
         printf("%d ", vettore[i]);
     }
     printf("\n");
 }
 
-int main(){
-    int n;
-    int *vettore=NULL;
-    int numeri;
-    richiesta();
-    vettore=creaVett(n);
-    Allocamento();
-    vettore=inserisciValori(vettore, n);
-    Aumento();
+int aumentaDimensione(int **vettore, int n) {
+    int scelta;
+    do {
+        printf("\nVuoi aumentare la dimensione? (1 = Si, 0 = No): ");
+        scanf("%d", &scelta);
+    } while (scelta != 0 && scelta != 1);
+
+    if (scelta == 0) {
+        printf("Ok,nessun nuovo elemento\n");
+        return n;
+    }
+
+    int nuova_dim;
+    printf("Nuova dimensione: ");
+    scanf("%d", &nuova_dim);
+
+    if (nuova_dim <= n) {
+        printf("La nuova dimensione è minore della prima\n");
+        exit(0);
+    }
+
+    *vettore = (int*)realloc(*vettore, nuova_dim * sizeof(int));
+    if (*vettore == NULL) {
+        printf("Errore reallocamento\n");
+        exit(1);
+    }
+
+    printf("Inserisci i nuovi valori:\n");
+    for (int i = n; i < nuova_dim; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &(*vettore)[i]);
+    }
+
+    return nuova_dim;
+}
+
+int main() {
+    int n = richiesta();
+    int *vettore = creaVett(n);
+    inserisciValori(vettore, n);
+    n = aumentaDimensione(&vettore, n);
     stampaVett(vettore, n);
+    free(vettore);
     return 0;
 }
