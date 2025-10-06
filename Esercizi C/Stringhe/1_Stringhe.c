006d4704fd84585f898b79c1f39d85d8e75d02c0
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2025-10-06 09:56:00 +0000

    uykykleddet

---
+++ Esercizi C/Stringhe/1_Stringhe.c
@@ -0,0 +1,28 @@
+/*
+Scrivi un programma in C che gestisca un insieme di numeri interi 
+usando un array dinamico.
+Il programma deve permettere, tramite menù:
+1. Aggiungereun numero (espandendo l'array con realloc)
+2. Visualizzare tutti i numeri
+3. Cercare un numero specifico
+4. Ordinare i numeri in ordine crescente
+5. Eliminare un numero scelto
+6. Uscire
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int* Aggiungi(int *vettore, int n){
+    int nuova_dim;
+    do{
+        printf("\nInserisci la nuova dimensione\n");
+        scanf("%d", &dim);
+    }while(dim<n);
+    *vettore = (int*)realloc(*vettore, nuova_dim * sizeof(int));
+}
+
+int main(){
+    int *vettore=NULL;
+    int *n=1;
+}
