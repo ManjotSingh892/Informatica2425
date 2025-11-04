0cecb4b1fb2075e5c40f09daa200910913b10901
ManjotSingh892 <manjot.singh1@galileiostiglia.edu.it>
2025-11-04 09:50:14 +0000

    Aggiunta del file Es_StringH_04_11_2025.c per la gestione del codice fiscale

---
+++ Esercizi C/Stringhe/Es_StringH_04_11_2025.c
@@ -0,0 +1,92 @@
+/*
+
+Codice fiscale
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef char* string;
+typedef int*  vettore;
+
+void Malloc(string stringa, int dim){
+    stringa=(char*)malloc(stringa, dim+1*sizeof(char));
+}
+
+
+
+void Realloc(string stringa, int NewDim){
+    stringa=(char*)realloc(stringa, NewDim*sizeof(char));
+}
+
+
+
+int CodiceFiscale(string nome, string cognome, int anni, char mese, int giorno, vettore CodComune, char Controllo){
+    int maxD=20;
+
+
+
+}
+
+
+
+int Cognome(string cognome, int maxD){     //Sistemare la funzione 
+    int cntC=0;
+    int cntV=0;
+    int j=0;
+    fgets(cognome, maxD, stdin);
+    int lenCognome=strlen(cognome);
+    char CCC[3];
+
+
+    for(int i=0; i<lenCognome; i++){        //corregere le if e rifare i for 
+        if(cognome[i]!='a' && 'e' && 'i' && 'o' && 'u'&&cntC<3){
+            cntC++;
+            CCC[j++]=cognome[i];
+        }else{
+            cntV++;
+        }
+    }
+
+    for(int i=0; i<lenCognome; i++){
+        if(cntC>2){
+            
+        }else if(cntV>2){
+            CCC[i]=cognome[i];
+        }
+    }
+    return cognome;
+}        
+
+
+
+
+int Nome(string nome, int maxD){
+    fgets(nome, maxD, stdin);
+
+}
+
+int età(int anni){
+    vettore anni;
+        anni= vettore[2]*10;
+        anni=anni + vettore[3];
+}
+
+int main(){
+    string cognome;
+    string nome;
+    int anno;
+    char mese;
+    int giorno;
+    string CodComune[]={71186}; //71=G in Ascii
+    char Controllo;
+
+
+
+
+
+
+    return 0; 
+}
